c55a6bd698db47f9a1676e8dbcb4f752f85cfc01
Павел <po-music@mail.ru>
2022-11-13 13:46:14 +0100

    Добавьте файлы проекта.

---
+++ Battleship/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Battleship
+{
+    //lines 80 chars
+    //files 200 lines
+    //no partial
+    //project 20 files
+
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
