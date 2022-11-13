dee4a33f755ec701a7f9730f91359275f385e68b
Павел <po-music@mail.ru>
2022-11-13 14:45:27 +0100

    Attack and win.

--- Battleship/Tests.cs
+++ Battleship/Tests.cs
@@ -7,19 +7,58 @@ namespace Battleship;
 
 public class Tests
 {
-    [TestCase(0, 0, true)]
-    [TestCase(1, 0, false)]
-    [TestCase(0, 1, false)]
-    public void AttackingAndExcluding(int shipLocation, int attackedLocation, 
-        bool expectedAnswer)
-    {
-        var excludedLocations = new List<int>();
-        
-        var hit = shipLocation == attackedLocation;
-        excludedLocations.Add(attackedLocation);
+    //todo tdd miss
+    //todo tdd throw if any location list is uninitialized
+    //todo tdd throw if two ships in same location
+    //todo tdd throw if ships are adjacent
 
-        Assert.That(hit, Is.EqualTo(expectedAnswer));
-        Assert.That(excludedLocations.Count, Is.EqualTo(1));
-        Assert.That(excludedLocations.Single(), Is.EqualTo(attackedLocation));
+    //todo similar for 2nd player
+    [Test]
+    public void Excluding()
+    {
+        player1Turn = true;
+        excludedLocations1 = CreateLocationList();
+
+        Hit(144);
+
+        Assert.That(excludedLocations1.Count, Is.EqualTo(1));
+        Assert.That(excludedLocations1.Single(), Is.EqualTo(144));
     }
+
+    [Test]
+    public void AttackAndWin()
+    {
+        player1Turn = true;
+        excludedLocations1 = CreateLocationList();
+        player1Ships = CreateLocationList(1);
+        player2Ships = CreateLocationList(2);
+
+        Hit(2);
+
+        Assert.That(player1Ships.Count, Is.EqualTo(1));
+        Assert.That(player1Ships.Single(), Is.EqualTo(1));
+        Assert.That(player2Ships.Count, Is.Zero);
+        Assert.That(hit);
+        Assert.That(win);
+        Assert.That(player1Turn);
+    }
+
+    private void Hit(int attackedLocation)
+    {
+        excludedLocations1.Add(attackedLocation);
+        player2Ships.Remove(attackedLocation);
+        hit = true;
+        win = true;
+    }
+
+    private List<int> CreateLocationList(params int[] locations) => 
+        locations.ToList();
+
+    private bool player1Turn = true;
+    private bool win;
+    private bool hit;
+    private List<int> excludedLocations1;
+    private List<int> excludedLocations2;
+    private List<int> player1Ships;
+    private List<int> player2Ships;
 }
