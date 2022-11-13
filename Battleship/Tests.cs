267db1336805ab7ced9d21fead96281e6adde540
Павел <po-music@mail.ru>
2022-11-13 13:53:00 +0100

    Simplest hit

---
+++ Battleship/Tests.cs
@@ -0,0 +1,16 @@
+namespace Battleship;
+
+//lines 80 chars
+//files 200 lines
+//no partial
+//project 20 files
+
+public class Tests
+{
+    [Test]
+    public void SimplestHit()
+    {
+        var hit = true;
+        Assert.That(hit);
+    }
+}
