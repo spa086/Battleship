484488e09956b364e2d7983d24e6a3678e035f8d
Павел <po-music@mail.ru>
2022-11-13 15:15:45 +0100

    Misses.

--- Battleship/Tests.cs
+++ Battleship/Tests.cs
@@ -7,56 +7,82 @@ namespace Battleship;
 
 public class Tests
 {
-    //todo tdd miss
     //todo tdd throw if any location list is uninitialized
     //todo tdd throw if two ships in same location
     //todo tdd throw if ships are adjacent
+    //todo tdd 2nd player turn
+    //todo tdd trying to attack the same place twice (not a valid turn)
+    //todo tdd field borders (and what is nowhere left to fire?)
+    //todo tdd 2nd dimension
+    //todo ASP project
+    //todo tdd multidecked ships (what about zigzag ships in 2 dimensions?)
+
+    [SetUp]
+    public void SetUp()
+    {
+        excludedLocations1 = CreateLocationList();
+        player1Turn = true;
+        win = false;
+    }
+
+    //todo similar for 2nd player
+    [Test]
+    public void Miss()
+    {
+        CreateShips(1, 2);
+
+        Attack(0);
+
+        Assert.False(player1Turn);
+        Assert.False(win);
+        Assert.That(player2Ships.Count, Is.EqualTo(1));
+    }
 
     //todo similar for 2nd player
     [Test]
     public void Excluding()
     {
-        player1Turn = true;
-        excludedLocations1 = CreateLocationList();
+        CreateShips(1, 2);
 
-        Hit(144);
+        Attack(144);
 
         Assert.That(excludedLocations1.Count, Is.EqualTo(1));
         Assert.That(excludedLocations1.Single(), Is.EqualTo(144));
     }
 
+    //todo similar for 2nd player
     [Test]
     public void AttackAndWin()
     {
-        player1Turn = true;
-        excludedLocations1 = CreateLocationList();
-        player1Ships = CreateLocationList(1);
-        player2Ships = CreateLocationList(2);
+        CreateShips(1, 2);
 
-        Hit(2);
+        Attack(2);
 
         Assert.That(player1Ships.Count, Is.EqualTo(1));
-        Assert.That(player1Ships.Single(), Is.EqualTo(1));
         Assert.That(player2Ships.Count, Is.Zero);
-        Assert.That(hit);
         Assert.That(win);
         Assert.That(player1Turn);
     }
 
-    private void Hit(int attackedLocation)
+    private void Attack(int attackedLocation)
     {
         excludedLocations1.Add(attackedLocation);
+        if (player2Ships.Contains(attackedLocation)) win = true;
+        else player1Turn = false;
         player2Ships.Remove(attackedLocation);
-        hit = true;
-        win = true;
     }
 
     private List<int> CreateLocationList(params int[] locations) => 
         locations.ToList();
 
+    private void CreateShips(int ship1Location, int ship2Location)
+    {
+        player1Ships = CreateLocationList(ship1Location);
+        player2Ships = CreateLocationList(ship2Location);
+    }
+
     private bool player1Turn = true;
     private bool win;
-    private bool hit;
     private List<int> excludedLocations1;
     private List<int> excludedLocations2;
     private List<int> player1Ships;
