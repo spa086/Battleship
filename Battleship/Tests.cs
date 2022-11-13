d1fd94cbc19fd21519803f05e85f47c44ca3d675
Павел <po-music@mail.ru>
2022-11-13 15:40:39 +0100

    Attacking the same place twice is prohibited.

--- Battleship/Tests.cs
+++ Battleship/Tests.cs
@@ -11,7 +11,6 @@ public class Tests
     //todo tdd throw if two ships in same location
     //todo tdd throw if ships are adjacent
     //todo tdd 2nd player turn
-    //todo tdd trying to attack the same place twice (not a valid turn)
     //todo tdd field borders (and what is nowhere left to fire?)
     //todo tdd 2nd dimension
     //todo ASP project
@@ -25,6 +24,16 @@ public class Tests
         win = false;
     }
 
+    [Test]
+    public void AttackSamePlaceTwice()
+    {
+        excludedLocations1 = CreateLocationList(1);
+
+        var exception = Assert.Throws<Exception>(() => Attack(1));
+        Assert.That(exception.Message, 
+            Is.EqualTo("Location [1] is already excluded."));
+    }
+
     //todo similar for 2nd player
     [Test]
     public void Miss()
@@ -33,9 +42,9 @@ public class Tests
 
         Attack(0);
 
-        Assert.False(player1Turn);
-        Assert.False(win);
-        Assert.That(player2Ships.Count, Is.EqualTo(1));
+        Assert.That(player1Turn, Is.False);
+        Assert.That(win, Is.False);
+        Assert.That(player2Ships!, Has.Count.EqualTo(1));
     }
 
     //todo similar for 2nd player
@@ -46,7 +55,7 @@ public class Tests
 
         Attack(144);
 
-        Assert.That(excludedLocations1.Count, Is.EqualTo(1));
+        Assert.That(excludedLocations1, Has.Count.EqualTo(1));
         Assert.That(excludedLocations1.Single(), Is.EqualTo(144));
     }
 
@@ -58,22 +67,22 @@ public class Tests
 
         Attack(2);
 
-        Assert.That(player1Ships.Count, Is.EqualTo(1));
-        Assert.That(player2Ships.Count, Is.Zero);
+        Assert.That(player1Ships, Has.Count.EqualTo(1));
+        Assert.That(player2Ships!, Is.Empty);
         Assert.That(win);
         Assert.That(player1Turn);
     }
 
     private void Attack(int attackedLocation)
     {
+        if (excludedLocations1.Contains(attackedLocation))
+            throw new Exception(
+                $"Location [{attackedLocation}] is already excluded.");
         excludedLocations1.Add(attackedLocation);
-        if (player2Ships.Contains(attackedLocation)) win = true;
+        if (player2Ships!.Contains(attackedLocation)) win = true;
         else player1Turn = false;
         player2Ships.Remove(attackedLocation);
     }
-
-    private List<int> CreateLocationList(params int[] locations) => 
-        locations.ToList();
 
     private void CreateShips(int ship1Location, int ship2Location)
     {
@@ -81,10 +90,12 @@ public class Tests
         player2Ships = CreateLocationList(ship2Location);
     }
 
-    private bool player1Turn = true;
+    private bool player1Turn;
     private bool win;
     private List<int> excludedLocations1;
-    private List<int> excludedLocations2;
-    private List<int> player1Ships;
-    private List<int> player2Ships;
+    private List<int>? player1Ships;
+    private List<int>? player2Ships;
+
+    private static List<int> CreateLocationList(params int[] locations) =>
+        locations.ToList();
 }
