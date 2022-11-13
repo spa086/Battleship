08f7b1f054b2cdc8f289e8b1cee42a8691081b13
Павел <po-music@mail.ru>
2022-11-13 14:02:48 +0100

    Attacking and excluding.

--- Battleship/Tests.cs
+++ Battleship/Tests.cs
@@ -7,12 +7,19 @@ namespace Battleship;
 
 public class Tests
 {
-    [Test]
-    public void SimplestHit()
+    [TestCase(0, 0, true)]
+    [TestCase(1, 0, false)]
+    [TestCase(0, 1, false)]
+    public void AttackingAndExcluding(int shipLocation, int attackedLocation, 
+        bool expectedAnswer)
     {
-        var shipLocation = 0;
-        var attackedLocation = 0;
+        var excludedLocations = new List<int>();
+        
         var hit = shipLocation == attackedLocation;
-        Assert.That(hit);
+        excludedLocations.Add(attackedLocation);
+
+        Assert.That(hit, Is.EqualTo(expectedAnswer));
+        Assert.That(excludedLocations.Count, Is.EqualTo(1));
+        Assert.That(excludedLocations.Single(), Is.EqualTo(attackedLocation));
     }
 }
