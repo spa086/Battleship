717ee9ad11a2552ece6be9b1f3ca182b3c589b9f
Павел <po-music@mail.ru>
2022-12-11 18:40:27 +0100

    Whatsup API method.

--- BattleshipApi/Program.cs
+++ BattleshipApi/Program.cs
@@ -8,14 +8,18 @@ public static class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
-        app.MapGet("/start", _ => Task.Run(() => new GamePool().StartGame()));
+        var pool = new GamePool();
+        app.MapGet("/start", _ => Task.Run(() => JsonSerializer.Serialize(pool.StartGame())));
+        app.MapGet("/whatsup", _ => Task.Run(() => JsonSerializer.Serialize(pool.WhatsUp())));
         app.Run();
     }
 }
 
 public class GamePool
 {
-    public void StartGame() => throw new NotImplementedException();
+    //true if game is started, false if we are waiting for second player to join.
+    public bool StartGame() => throw new NotImplementedException();
+    //true if its requester's turn, false if we are waiting for the game to start
+    public bool WhatsUp() => throw new NotImplementedException();
 }
-
 
