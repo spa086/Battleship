f2470bd7b36d4bb59f4c0781a365114ff1d37952
Pavel Onishenko <pavelonishchenko.software@gmail.com>
2023-04-04 13:21:40 +0200

    Reorganization.

--- BattleShipLibrary/Game.cs
+++ BattleShipLibrary/Game.cs
@@ -161,33 +161,3 @@ public class Game
                 "Target cannot be outside the game field. Available coordinates are 0-9.");
     }
 }
-
-public class TimerPlus : IDisposable
-{
-    public TimerPlus(TimerCallback callback, object state, TimeSpan dueTime, TimeSpan period)
-    {
-        timer = new System.Threading.Timer(Callback, state, dueTime, period);
-        realCallback = callback;
-        this.period = period;
-        next = DateTime.Now.Add(dueTime);
-    }
-
-    public TimeSpan DueTime => next - DateTime.Now;
-
-    public void Dispose()
-    {
-        timer.Dispose();
-        GC.SuppressFinalize(this);
-    }
-
-    private void Callback(object? state)
-    {
-        next = DateTime.Now.Add(period);
-        realCallback(state);
-    }
-
-    private readonly TimerCallback realCallback;
-    private readonly System.Threading.Timer timer;
-    private readonly TimeSpan period;
-    private DateTime next;
-}
