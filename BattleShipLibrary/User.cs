584a99b74a302410b902949e756f32a7b09b29d2
Pavel Onishenko <pavelonishchenko.software@gmail.com>
2023-04-23 00:56:49 +0200

    Refactoring.

--- BattleShipLibrary/User.cs
+++ BattleShipLibrary/User.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BattleshipLibrary;
+﻿namespace BattleshipLibrary;
 
 public class User
 {
-    public bool IsBot { get; set; }
+    public bool IsBot { get; init; }
     public int Id { get; set; }
 
-    //todo persist user name between games
     public string? Name { get; set; }
 
-    public List<Cell> ExcludedLocations { get; set; } = new List<Cell>();
+    public List<Cell> ExcludedLocations { get; set; } = new();
 
     //todo tdd validate ship shape
     public Ship[]? Fleet { get; set; }
-
 }
