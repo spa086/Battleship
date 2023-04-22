8f2853b49c354d97dd0254c44f8d7ffd0d8bb654
Pavel Onishenko <pavelonishchenko.software@gmail.com>
2023-04-23 01:32:39 +0200

    Refactoring.

--- Battleship/TestableGame.cs
+++ Battleship/TestableGame.cs
@@ -10,15 +10,13 @@ public class TestableGame : Game
 
     public TimerWithDueTime? Timer => TheTimer;
 
-    public TestableGame CreateGuest(int? guestId = null)
+    public void CreateGuest(int? guestId = null)
     {
         if (guestId is not null)
         {
-            if (Guest is null) Guest = new User();
+            Guest ??= new User();
             Guest.Id = guestId.Value;
         }
-
-        return this;
     }
 
     public TestableGame SetState(GameState newState)
@@ -30,7 +28,6 @@ public class TestableGame : Game
     public TimerWithDueTime? GetTimer() => TheTimer;
 
     public int? SetupTurnTime { get; set; }
-    public int? SetupMatchingTime { get; set; }
 
     public void EnqueueAiAttackLocation(Cell location) =>
         (Ai as TestAi)!.AttackLocationsQueue.Enqueue(location);
@@ -52,7 +49,8 @@ public class TestableGame : Game
         Guest = new User { ExcludedLocations = CreateLocationList() };
         State = GameState.HostTurn;
         SetupTurnTime = 30;
-        SetupSimpleFleets(new[] { new Cell(1, 1) }, 1, new[] { new Cell(3, 3) }, 2);
+        SetupSimpleFleets(new[] { new Cell(1, 1) }, 1,
+            new[] { new Cell(3, 3) }, 2);
     }
 
     public void SetupFleets(IEnumerable<Ship>? hostFleet, IEnumerable<Ship>? guestFleet)
@@ -63,15 +61,14 @@ public class TestableGame : Game
 
     public void DestroyFleet(int userId)
     {
-        if (userId == Host.Id)
-            foreach (var ship in Host.Fleet!)
-            foreach (var deck in ship.Decks.Values)
-                deck.Destroyed = true;
+        if (userId != Host.Id) return;
+        foreach (var ship in Host.Fleet!)
+        foreach (var deck in ship.Decks.Values)
+            deck.Destroyed = true;
     }
 
     public void SetupBattleTimer(int secondsLeft) => SetBattleTimer(secondsLeft);
     public void SetupShipsCreationTimer(int secondsLeft) => SetShipsCreationTimer(secondsLeft);
-    public void SetupMatchingTimer(int secondsLeft) => SetShipsCreationTimer(secondsLeft);
 
     public void SetupUserName(int userId, string? userName)
     {
@@ -81,10 +78,10 @@ public class TestableGame : Game
     }
 
     //todo move id setups to different method/property
-    public void SetupSimpleFleets(Cell[]? hostDeckLocations, int? hostId = null,
-        Cell[]? guestDeckLocations = null, int? guestId = null)
+    public void SetupSimpleFleets(Cell[]? hostDeckLocations, int? hostId = null, Cell[]? guestDeckLocations = null, 
+        int? guestId = null)
     {
-        Host!.Fleet = CreateSimpleFleet(hostDeckLocations);
+        Host.Fleet = CreateSimpleFleet(hostDeckLocations);
         Host.Id = hostId ?? Host.Id;
         Guest!.Fleet = CreateSimpleFleet(guestDeckLocations);
         if (guestId is null) Guest = null;
@@ -97,15 +94,11 @@ public class TestableGame : Game
     protected override void SetShipsCreationTimer(int secondsLeft = 30) =>
         base.SetShipsCreationTimer(SetupTurnTime ?? secondsLeft);
 
-    protected override void SetMatchingTimer(int secondsLeft = 30) =>
-        base.SetMatchingTimer(SetupMatchingTime ?? secondsLeft);
-
     private static Ship[]? CreateSimpleFleet(Cell[]? deckLocations)
     {
-        if (deckLocations is null)
-            return null;
-        var decks = deckLocations.Select(location => new Deck(location.X, location.Y))
-            .ToDictionary(x => x.Location);
+        if (deckLocations is null) return null;
+        var decks = deckLocations
+            .Select(location => new Deck(location.X, location.Y)).ToDictionary(x => x.Location);
         return new[] { new Ship { Decks = decks } };
     }
 
